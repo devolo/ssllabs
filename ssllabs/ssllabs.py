a82eae37d5c120f51dc211f8ed16fe1483120add
Guido Schmitz <guido.schmitz@fedaix.de>
2021-01-21 21:03:25 +0100

    Move high level methods

---
+++ ssllabs/ssllabs.py
@@ -0,0 +1,16 @@
+import asyncio
+
+from .api.analyze import Analyze
+
+
+class Ssllabs():
+
+    async def analyze(self, host: str):
+        api = Analyze()
+        ready = False
+        host_object = None
+        while not ready:
+            host_object = await api.get(host=host)
+            ready = host_object.status == "READY"
+            await asyncio.sleep(10)
+        return host_object
