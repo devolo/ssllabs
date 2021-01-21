f50e16be49e1e4087a4d7ea42b7a12cf83ea1f4c
Markus Bong <Markus.Bong@devolo.de>
2021-01-21 11:28:13 +0100

    use instance of httpx.AsyncClient

--- ssllabs/ssllabs.py
+++ ssllabs/ssllabs.py
@@ -1,15 +1,20 @@
 import httpx
 import time
+import asyncio
 
 SSLLABS_URL = "https://api.ssllabs.com/api/v3/"
 
 class Ssllabs:
     def __init__(self, url):
         self.url = url
+        self.client = httpx.AsyncClient()
         
+    def __del__(self):
+        loop = asyncio.get_event_loop()
+        loop.create_task(self.httpx.aclose())
+
     async def available(self):
-        async with httpx.AsyncClient() as c:
-            return (await c.get(f"{SSLLABS_URL}/info")).json()
+        return (await self.client.get(f"{SSLLABS_URL}/info")).json()
     
     async def start_assessment(self, publish: bool = False, start_new: bool = True): 
         """[summary]
@@ -18,15 +23,13 @@ class Ssllabs:
             [type]: [description]
         """
         data = {"host": self.host, "publish": publish, "start_new": start_new}
-        async with httpx.AsyncClient() as c:
-            return await c.get(f"{SSLLABS_URL}/analyze", params=data)
+        return await self.client.get(f"{SSLLABS_URL}/analyze", params=data)
     
     async def get_assessment(self):
         start_time = time.time()
-        async with httpx.AsyncClient() as c:
-            while (await c.get(f"{SSLLABS_URL}/analyze", params={"host": self.url})).json()["status"] == "IN_PROGRESS": 
-                if time.time() > start_time + 600:
-                    raise ValueError("CHANGE IT TO A TIMEOUT")
-                print("waiting for result")
-                time.sleep(5)
-            return (await c.get(f"{SSLLABS_URL}/analyze", params={"host": self.url})).json()
+        while (await self.client.get(f"{SSLLABS_URL}/analyze", params={"host": self.url})).json()["status"] == "IN_PROGRESS": 
+            if time.time() > start_time + 600:
+                raise ValueError("CHANGE IT TO A TIMEOUT")
+            print("waiting for result")
+            time.sleep(5)
+        return (await self.client.get(f"{SSLLABS_URL}/analyze", params={"host": self.url})).json()
