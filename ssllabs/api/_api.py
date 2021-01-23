2ecf9e32536d344cc00a2e3aa19c24efa05a45b5
Guido Schmitz <guido.schmitz@fedaix.de>
2021-01-23 22:04:57 +0100

    Raise on HTTP errors

--- ssllabs/api/_api.py
+++ ssllabs/api/_api.py
@@ -9,9 +9,10 @@ SSLLABS_URL = f"https://api.ssllabs.com/api/v{API_VERSION}/"
 
 
 class _Api(ABC):
+    """Abstract class to communicate with Qualys SSL Labs Assessment APIs."""
 
     def __init__(self):
-        self.logger = logging.getLogger()
+        self.logger = logging.getLogger(f"{self.__class__.__module__}.{self.__class__.__name__}")
         self.client = httpx.AsyncClient()
 
     def __del__(self):
@@ -19,4 +20,7 @@ class _Api(ABC):
         loop.create_task(self.client.aclose())
 
     async def _call(self, api_endpoint: str, **kwargs):
-        return await self.client.get(f"{SSLLABS_URL}{api_endpoint}", params=kwargs)
+        """Invocate API."""
+        r = await self.client.get(f"{SSLLABS_URL}{api_endpoint}", params=kwargs)
+        r.raise_for_status()
+        return r
