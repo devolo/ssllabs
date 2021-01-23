877cdc340000e2a1804698a90a0aae4f61d43448
Guido Schmitz <guido.schmitz@fedaix.de>
2021-01-23 11:31:37 +0100

    Optimize analyze methode

--- ssllabs/ssllabs.py
+++ ssllabs/ssllabs.py
@@ -7,10 +7,8 @@ class Ssllabs():
 
     async def analyze(self, host: str):
         api = Analyze()
-        ready = False
         host_object = await api.get(host=host)
-        while not ready:
+        while host_object.status not in ["READY", "ERROR"]:
             await asyncio.sleep(10)
             host_object = await api.get(host=host)
-            ready = host_object.status == "READY"  # ToDo: Handle status ERROR
         return host_object
