c08bdb53cc7d1964ac3398130da74e1bc3ce4824
Guido Schmitz <guido.schmitz@fedaix.de>
2021-01-24 11:50:42 +0100

    Make more fields optional

--- ssllabs/data/endpoint.py
+++ ssllabs/data/endpoint.py
@@ -14,7 +14,7 @@ class EndpointData:
     ipAddress: str
     """Endpoint IP address, in IPv4 or IPv6 format."""
 
-    serverName: str
+    serverName: Optional[str]
     """Server name retrieved via reverse DNS"""
 
     statusMessage: str
@@ -29,24 +29,24 @@ class EndpointData:
     grade: Optional[str]
     """Possible values: A+, A-, A-F, T (no trust) and M (certificate name mismatch)"""
 
-    gradeTrustIgnored: str
+    gradeTrustIgnored: Optional[str]
     """Grade (as above), if trust issues are ignored"""
 
     futureGrade: Optional[str]
     """Next grade because of upcoming grading criteria changes, Null if there is no impact on current grade."""
 
-    hasWarnings: bool
+    hasWarnings: Optional[bool]
     """If this endpoint has warnings that might affect the score (e.g., get A- instead of A)."""
 
-    isExceptional: bool
+    isExceptional: Optional[bool]
     """
     This flag will be raised when an exceptional configuration is encountered. The SSL Labs test will give such sites an A+.
     """
 
-    progress: int
+    progress: Optional[int]
     """Assessment progress, which is a value from 0 to 100, and -1 if the assessment has not yet started"""
 
-    duration: int
+    duration: Optional[int]
     """Assessment duration, in milliseconds"""
 
     eta: Optional[int]
