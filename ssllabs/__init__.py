4ffd7a1666ae01e59a3e7d47de14003cd1117ce1
Guido Schmitz <guido.schmitz@fedaix.de>
2021-01-25 09:43:45 +0100

    Fix CI

--- ssllabs/__init__.py
+++ ssllabs/__init__.py
@@ -1,4 +1,10 @@
+from .ssllabs import Ssllabs
+
 __all__ = ['Ssllabs']
 __version__ = "0.1.0"
 
-from .ssllabs import Ssllabs
+__locals = locals()
+
+for _name in __all__:
+    if not _name.startswith("__"):
+        setattr(__locals[_name], "__module__", "ssllabs")
