b9d802392b1b32dd54b8b2432cfe23336c1e8383
Markus Bong <Markus.Bong@devolo.de>
2021-01-26 15:55:41 +0100

    add more tests

--- tests/test_api__api.py
+++ tests/test_api__api.py
@@ -8,4 +8,22 @@ try:
     from unittest.mock import AsyncMock
 except ImportError:
     from asynctest import CoroutineMock as AsyncMock
-from httpx import Response
+
+from httpx import HTTPStatusError, Request, Response
+
+
+class TestAPI:
+
+    @pytest.mark.asyncio
+    async def test_api(self):
+        req = Request("GET", "")
+        with patch("httpx._client.AsyncClient.get", new=AsyncMock(return_value=Response(401, request=req))):
+            with pytest.raises(HTTPStatusError):
+                await _Api()._call("")
+
+    @pytest.mark.asyncio
+    async def test_api_positive(self):
+        req = Request("GET", "")
+        with patch("httpx._client.AsyncClient.get", new=AsyncMock(return_value=Response(200, request=req))):
+            r = await _Api()._call("")
+            assert type(r) is Response
--- tests/test_ssllabs.py
+++ tests/test_ssllabs.py
@@ -4,6 +4,7 @@ import pytest
 from dacite import from_dict
 
 from ssllabs import Ssllabs
+from ssllabs.data.info import InfoData
 from ssllabs.data.status_codes import StatusCodesData
 
 try:
@@ -23,7 +24,7 @@ class TestSsllabs:
             assert root_certs == request.cls.root_certs["rootCerts"]
 
     @pytest.mark.asyncio
-    async def test_root_certs_value_error(self, request):
+    async def test_root_certs_value_error(self):
         with pytest.raises(ValueError):
             ssllabs = Ssllabs()
             await ssllabs.root_certs(trust_store=6)
@@ -36,3 +37,19 @@ class TestSsllabs:
             ssllabs = Ssllabs()
             status_codes = await ssllabs.status_codes()
             assert status_codes.statusDetails == request.cls.status_details["statusDetails"]
+
+    @pytest.mark.asyncio
+    async def test_availability(self, request):
+        with patch("ssllabs.api.info.Info.get",
+                   new=AsyncMock(return_Value=from_dict(data_class=InfoData,
+                                                        data=request.cls.api_info))):
+            ssllabs = Ssllabs()
+            assert await ssllabs.availability()
+
+    @pytest.mark.asyncio
+    async def test_availability_http_error(self):
+        from httpx import Request, Response
+        req = Request("GET", "")
+        with patch("httpx._client.AsyncClient.get", new=AsyncMock(return_value=Response(401, request=req))):
+            ssllabs = Ssllabs()
+            assert not await ssllabs.availability()
