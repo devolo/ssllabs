bc43ba12b8d7ebaf3d7b8891cb2fa0f7dfffc70c
Markus Bong <Markus.Bong@devolo.de>
2021-01-26 13:57:31 +0100

    prevent tests failing on nested datclasses

--- tests/test_api_endpoint.py
+++ tests/test_api_endpoint.py
@@ -3,9 +3,13 @@ import json
 
 import pytest
 
+from ssllabs.api.analyze import Analyze
 from ssllabs.api.endpoint import Endpoint
+from ssllabs.api.info import Info
 from ssllabs.api.status_codes import StatusCodes
 from ssllabs.data.endpoint import EndpointData
+from ssllabs.data.host import HostData
+from ssllabs.data.info import InfoData
 from ssllabs.data.status_codes import StatusCodesData
 
 
@@ -16,12 +20,22 @@ class TestEndpoint:
                   EndpointData,
                   {
                       "host": "a",
-                      "endpoint_ip": "b"
+                      "s": "b"
                   }),
                  ("status_details",
                   StatusCodes,
                   StatusCodesData,
-                  {})]
+                  {}),
+                 ("api_info",
+                  Info,
+                  InfoData,
+                  {}),
+                 ("analyze",
+                  Analyze,
+                  HostData,
+                  {
+                      "host": "devolo.de"
+                  })]
 
     @pytest.mark.asyncio
     @pytest.mark.parametrize("path, class_name, dataclass_name, function_parameter", test_data)
@@ -32,6 +46,13 @@ class TestEndpoint:
         assert type(endpoint) is dataclass_name
         for key, value in getattr(request.cls, path).items():
             if not dataclasses.is_dataclass(getattr(endpoint, key)):
-                assert value == getattr(endpoint, key)
+                if not type(getattr(endpoint, key)) is list:
+                    assert value == getattr(endpoint, key)
+                else:
+                    if not all([dataclasses.is_dataclass(e) for e in getattr(endpoint, key)]):
+                        assert value == getattr(endpoint, key)
+                    else:
+                        # TODO:
+                        """Handle this!"""
             else:
                 assert dataclasses.is_dataclass(getattr(endpoint, key))
