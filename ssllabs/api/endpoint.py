b69c5c1a0c167cb6f65609e8cac09b3971d1772a
Guido Schmitz <guido.schmitz@fedaix.de>
2021-01-26 13:52:03 +0100

    Rename parameter to match docs

--- ssllabs/api/endpoint.py
+++ ssllabs/api/endpoint.py
@@ -10,14 +10,14 @@ class Endpoint(_Api):
     See also: https://github.com/ssllabs/ssllabs-scan/blob/master/ssllabs-api-docs-v3.md#retrieve-detailed-endpoint-information
     """
 
-    async def get(self, host: str, endpoint_ip: str, **kwargs) -> EndpointData:
+    async def get(self, host: str, s: str, **kwargs) -> EndpointData:
         """Retrieve detailed endpoint information.
 
         :param host: Hostname to analyze
-        :param endpoint_ip: Endpoint IP address
+        :param s: Endpoint IP address
         :key fromCache: Always deliver cached assessment reports if available; optional, defaults to "off". This parameter is
                         intended for API consumers that don't want to wait for assessment results. Can't be used at the same
                         time as the startNew parameter.
         """
-        r = await self._call("getEndpointData", host=host, s=endpoint_ip, **kwargs)
+        r = await self._call("getEndpointData", host=host, s=s, **kwargs)
         return from_dict(data_class=EndpointData, data=r.json())
