ad9543556ba7a05f600e75e0b46cb0c303d3ec79
Markus Bong <Markus.Bong@devolo.de>
2021-01-26 12:53:24 +0100

    implement a general testmethod

--- tests/test_api_endpoint.py
+++ tests/test_api_endpoint.py
@@ -1,16 +1,37 @@
+import dataclasses
 import json
 
 import pytest
 
 from ssllabs.api.endpoint import Endpoint
+from ssllabs.api.status_codes import StatusCodes
 from ssllabs.data.endpoint import EndpointData
+from ssllabs.data.status_codes import StatusCodesData
 
 
 class TestEndpoint:
 
+    test_data = [("endpoint",
+                  Endpoint,
+                  EndpointData,
+                  {
+                      "host": "a",
+                      "endpoint_ip": "b"
+                  }),
+                 ("status_details",
+                  StatusCodes,
+                  StatusCodesData,
+                  {})]
+
     @pytest.mark.asyncio
-    async def test_endpoint(self, request, patch_httpx):
-        patch_httpx.return_value._text = json.dumps(request.cls.endpoint)
-        e = Endpoint()
-        endpoint = await e.get("a", "b")
-        assert type(endpoint) is EndpointData
+    @pytest.mark.parametrize("path, class_name, dataclass_name, function_parameter", test_data)
+    async def test_endpoint(self, request, patch_httpx, path, class_name, dataclass_name, function_parameter):
+        patch_httpx.return_value._text = json.dumps(getattr(request.cls, path))
+        e = class_name()
+        endpoint = await e.get(**function_parameter)
+        assert type(endpoint) is dataclass_name
+        for key, value in getattr(request.cls, path).items():
+            if not dataclasses.is_dataclass(getattr(endpoint, key)):
+                assert value == getattr(endpoint, key)
+            else:
+                assert dataclasses.is_dataclass(getattr(endpoint, key))
