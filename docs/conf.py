10e5f67f4d2065697d1b294cf103fbf6bd832faa
Guido Schmitz <guido.schmitz@fedaix.de>
2021-03-23 10:50:36 +0000

    Add github url

--- docs/conf.py
+++ docs/conf.py
@@ -41,6 +41,9 @@ exclude_patterns = ['_build', "README.md"]
 # a list of builtin themes.
 #
 html_theme = 'pydata_sphinx_theme'
+html_theme_options = {
+    "github_url": "https://github.com/devolo/ssllabs",
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
