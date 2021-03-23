180b5005326bc27517aa2a45ecb96b7285682906
Guido Schmitz <guido.schmitz@fedaix.de>
2021-03-23 13:13:57 +0000

    Fix package name

--- docs/conf.py
+++ docs/conf.py
@@ -14,37 +14,37 @@ import os
 import sys
 from pkg_resources import get_distribution
 
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath("../"))
 
-version = get_distribution(__package__).version
+version = get_distribution("ssllabs").version
 
 # -- Project information -----------------------------------------------------
 
-project = get_distribution(__package__).project_name
-copyright = '2021, devolo AG'
-author = 'Markus Bong, Guido Schmitz'
+project = get_distribution("ssllabs").project_name
+copyright = "2021, devolo AG"
+author = "Markus Bong, Guido Schmitz"
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# extensions coming with Sphinx (named "sphinx.ext.*") or your custom
 # ones.
 extensions = ["sphinx.ext.autosummary", "m2r2"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', "README.md"]
+exclude_patterns = ["_build", "README.md"]
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'pydata_sphinx_theme'
+html_theme = "pydata_sphinx_theme"
 html_theme_options = {
     "github_url": "https://github.com/devolo/ssllabs",
 }
@@ -52,4 +52,4 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+# html_static_path = ["_static"]
