643c5b3635f5b127fe2a3ccca6779e2864e85500
Guido Schmitz <guido.schmitz@fedaix.de>
2023-09-11 21:39:53 +0200

    Fix version detection for Python 3.12

--- ssllabs/__init__.py
+++ ssllabs/__init__.py
@@ -1,12 +1,13 @@
-from pkg_resources import DistributionNotFound, get_distribution
+"""Qualys SSL Labs API in Python."""
+from importlib.metadata import PackageNotFoundError, version
 
 from .ssllabs import Ssllabs
 
 __license__ = "MIT"
 
 try:
-    __version__ = get_distribution(__package__).version
-except DistributionNotFound:
+    __version__ = version("ssllabs")
+except PackageNotFoundError:
     # package is not installed
     __version__ = "0.0.0"
 
